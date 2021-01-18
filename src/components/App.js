87d8d1b94562ae1c14082868663ffe49d829a643
adrian moreno <morenoadrianh1.2@gmail.com>
2021-01-18 18:36:23 -0500

    agregando suspense

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,41 @@
-import React, { useContext, useEffect } from 'react'
-import { GlobalStyle } from './GlobalStyles'
-import Header from './Header'
-import { Home } from './Home'
-import { Router, Redirect } from '@reach/router'
-import { Detail } from '../pages/Detail'
-import { MovieList } from './MovieList'
-import { Results } from './Results'
-import { MobileMenu } from './menu'
-import { Register } from '../pages/Register'
-import { User } from '../pages/User'
-import { Context } from '../Context/ContextAuth'
-import { WatchLater } from '../pages/watchLater'
+import React, { useContext, useEffect, Suspense } from "react";
+import { GlobalStyle } from "./GlobalStyles";
+import Header from "./Header";
+import { Home } from "./Home";
+import { Router, Redirect } from "@reach/router";
+import { Detail } from "../pages/Detail";
+import { MovieList } from "./MovieList";
+import { Results } from "./Results";
+import { MobileMenu } from "./menu";
+import { Register } from "../pages/Register";
+import { User } from "../pages/User";
+import { Context } from "../Context/ContextAuth";
+import { WatchLater } from "../pages/watchLater";
 
 export default () => {
-  const { isAuth } = useContext(Context)
-    useEffect(()=>{
-    localStorage.clear()
-  },[])
+  const { isAuth } = useContext(Context);
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Header />
 
       <Router>
-        <Home path='/home/' />
-        <Home path='/' />
-        <Detail path='/detail/:id/' />
-        <MovieList path='/category/:category/' />
-        <Results path='/results/:search' />
-        {!isAuth && <Redirect noThrow from='/watchlater' to='/register' />}
-        {!isAuth && <Redirect noThrow from='/user' to='/register' />}
-        {isAuth && <Redirect noThrow from='/register' to='/' />}
-        <User path='/user' />
-        <WatchLater path='/watchlater' />
-        <Register path='/register' />
+        <Home path="/home/" />
+        <Home path="/" />
+        <Detail path="/detail/:id/" />
+        <MovieList path="/category/:category/" />
+        <Results path="/results/:search" />
+        {!isAuth && <Redirect noThrow from="/watchlater" to="/register" />}
+        {!isAuth && <Redirect noThrow from="/user" to="/register" />}
+        {isAuth && <Redirect noThrow from="/register" to="/" />}
+        <User path="/user" />
+        <WatchLater path="/watchlater" />
+        <Register path="/register" />
       </Router>
-      <MobileMenu/ >
-
-    </>
-  )
-}
+      <MobileMenu />
+    </Suspense>
+  );
+};
