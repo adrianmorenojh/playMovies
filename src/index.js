0a515380b08f04cb518518b01c1781136621c2e4
adrian moreno <morenoadrianh1.2@gmail.com>
2021-01-18 20:01:39 -0500

    implementando hash history

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDom from "react-dom";
 import App from "./components/App";
 import Context from "./Context/ContextAuth";
 
+import { LocationProvider, createHistory } from "@reach/router";
+import { createHashSource } from "reach-router-hash-history";
+
+const history = createHistory(createHashSource());
+
 ReactDom.render(
   <Context.Provider>
-    <App />
+    <LocationProvider history={history}>
+      <App />
+    </LocationProvider>
   </Context.Provider>,
   document.getElementById("app")
 );
